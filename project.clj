143509781e08ce8938ba28aad02c65c2594fb747
Christopher Thonfeld-Guckes <christopher.guckes@torq-dev.de>
2020-12-07 21:53:33 +0100

    Added nop-logger dependency, removed unused test file

--- project.clj
+++ project.clj
@@ -1,13 +1,14 @@
 (defproject clocr "1.0.0-SNAPSHOT"
-            :description "FIXME: write description"
-            :url "http://example.com/FIXME"
-            :license {:name "MIT"
-                      :url  "https://todo"}
-            :dependencies [[org.clojure/clojure "1.10.1"]
-                           [net.sourceforge.tess4j/tess4j "4.5.4"]]
-            :min-lein-version "2.9.0"
-            :source-paths ["src"]
-            :test-paths ["test"]
-            :resource-paths ["resources"]
-            :dev {:resource-paths ["test-resources"]}
-            :repl-options {:init-ns clocr.core})
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "MIT"
+            :url  "https://todo"}
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [net.sourceforge.tess4j/tess4j "4.5.4"]
+                 [org.slf4j/slf4j-nop "1.7.30"]]
+  :min-lein-version "2.9.0"
+  :source-paths ["src"]
+  :test-paths ["test"]
+  :resource-paths ["resources"]
+  :dev {:resource-paths ["test-resources"]}
+  :repl-options {:init-ns clocr.core})
--- test/clocr/core_test.clj
+++
@@ -1,6 +0,0 @@
-(ns clocr.core-test
-  (:require [clojure.test :refer :all]
-            [clocr.core :refer :all]))
-
-(deftest init-works
-  (is (not (nil? (init "deu")))))
